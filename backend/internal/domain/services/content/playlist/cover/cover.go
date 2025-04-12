25857c1a583b4e2a5af150309f4ccc5ad58003dd
hahaclassic <gavrilyukvlad221@gmail.com>
2025-04-13 01:23:49 +0300

    feature/business-layer: playlist cover service

---
+++ backend/internal/domain/services/content/playlist/cover/cover.go
@@ -0,0 +1,86 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/hahaclassic/orpheon/backend/internal/domain/entity"
+	"github.com/hahaclassic/orpheon/backend/pkg/errwrap"
+)
+
+var (
+	ErrUploadCover = errors.New("upload cover error")
+	ErrGetCover    = errors.New("get cover error")
+	ErrDeleteCover = errors.New("delete cover error")
+
+	ErrForbidden = errors.New("access forbidden")
+)
+
+type PlaylistCoverRepository interface {
+	SaveCover(ctx context.Context, cover *entity.Cover) error
+	GetCover(ctx context.Context, playlistID uuid.UUID) (*entity.Cover, error)
+	DeleteCover(ctx context.Context, playlistID uuid.UUID) error
+}
+
+type PlaylistPolicyService interface {
+	CanDelete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) (bool, error)
+	CanEdit(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) (bool, error)
+	CanView(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) (bool, error)
+}
+
+type PlaylistCoverService struct {
+	policy PlaylistPolicyService
+	repo   PlaylistCoverRepository
+}
+
+func New(repo PlaylistCoverRepository) *PlaylistCoverService {
+	return &PlaylistCoverService{
+		repo: repo,
+	}
+}
+
+func (c *PlaylistCoverService) GetCover(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) (*entity.Cover, error) {
+	canUpdate, err := c.policy.CanEdit(ctx, claims, playlistID)
+	if err != nil {
+		return nil, errwrap.Wrap(ErrGetCover, err)
+	}
+	if !canUpdate {
+		return nil, errwrap.Wrap(ErrGetCover, ErrForbidden)
+	}
+
+	cover, err := c.repo.GetCover(ctx, playlistID)
+	if err != nil {
+		return nil, errwrap.Wrap(ErrGetCover, err)
+	}
+
+	return cover, nil
+}
+
+func (c *PlaylistCoverService) UploadCover(ctx context.Context, claims *entity.Claims, cover *entity.Cover) error {
+	canUpdate, err := c.policy.CanEdit(ctx, claims, cover.ObjectID)
+	if err != nil {
+		return errwrap.Wrap(ErrUploadCover, err)
+	}
+	if !canUpdate {
+		return errwrap.Wrap(ErrUploadCover, ErrForbidden)
+	}
+
+	err = c.repo.SaveCover(ctx, cover)
+
+	return errwrap.WrapIfErr(ErrUploadCover, err)
+}
+
+func (c *PlaylistCoverService) DeleteCover(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error {
+	canUpdate, err := c.policy.CanDelete(ctx, claims, playlistID)
+	if err != nil {
+		return errwrap.Wrap(ErrDeleteCover, err)
+	}
+	if !canUpdate {
+		return errwrap.Wrap(ErrDeleteCover, ErrForbidden)
+	}
+
+	err = c.repo.DeleteCover(ctx, playlistID)
+
+	return errwrap.WrapIfErr(ErrDeleteCover, err)
+}
--- backend/internal/domain/services/content/playlist/deleter/deleter.go
+++ backend/internal/domain/services/content/playlist/deleter/deleter.go
@@ -10,30 +10,36 @@ import (
 // TODO: нет транзакции. Нет восстанавливающих операций. Надо продумать,
 // как сделать так, чтобы сохранялась целостность данных.
 
-type metaDeletionService interface {
+type MetaDeletionService interface {
 	Delete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error
 }
 
-type trackDeletionService interface {
+type TrackDeletionService interface {
 	Delete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error
 }
 
-type favoritesDeletionService interface {
+type FavoritesDeletionService interface {
 	Delete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error
+}
+
+type PlaylistCoverDeletionService interface {
+	Delete(ctx context.Context, claims *entity.Claims, objectID uuid.UUID) error
 }
 
 type PlaylistDeleter struct {
-	meta      metaDeletionService
-	tracks    trackDeletionService
-	favorites favoritesDeletionService
+	meta      MetaDeletionService
+	tracks    TrackDeletionService
+	favorites FavoritesDeletionService
+	cover     PlaylistCoverDeletionService
 }
 
-func New(meta metaDeletionService, track trackDeletionService,
-	favorites favoritesDeletionService) *PlaylistDeleter {
+func New(meta MetaDeletionService, track TrackDeletionService,
+	favorites FavoritesDeletionService, cover PlaylistCoverDeletionService) *PlaylistDeleter {
 	return &PlaylistDeleter{
 		meta:      meta,
 		tracks:    track,
 		favorites: favorites,
+		cover:     cover,
 	}
 }
 
@@ -50,8 +56,60 @@ func (p *PlaylistDeleter) Delete(ctx context.Context, claims *entity.Claims, pla
 
 	err = p.meta.Delete(ctx, claims, playlistID)
 	if err != nil {
-
 		return err
+	}
+
+	err = p.cover.Delete(ctx, claims, playlistID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type DeletionService interface {
+	Delete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error
+}
+
+type PlaylistDeleterV2 struct {
+	deleters []DeletionService
+}
+
+func NewV2(deleters []DeletionService) *PlaylistDeleterV2 {
+	return &PlaylistDeleterV2{
+		deleters: deleters,
+	}
+}
+
+func (p *PlaylistDeleterV2) Delete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error {
+	for i := range p.deleters {
+		err := p.deleters[i].Delete(ctx, claims, playlistID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type Deleter func(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error
+
+type PlaylistDeleterV3 struct {
+	deleters []Deleter
+}
+
+func NewV3(deleters []Deleter) *PlaylistDeleterV3 {
+	return &PlaylistDeleterV3{
+		deleters: deleters,
+	}
+}
+
+func (p *PlaylistDeleterV3) Delete(ctx context.Context, claims *entity.Claims, playlistID uuid.UUID) error {
+	for i := range p.deleters {
+		err := p.deleters[i](ctx, claims, playlistID)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
