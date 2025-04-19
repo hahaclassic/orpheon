4d83d8c4dea998b444df26fc330d6b7f1be2f3c1
hahaclassic <gavrilyukvlad221@gmail.com>
2025-04-19 20:01:51 +0300

    feature/business-layer: added search service for content

---
+++ backend/internal/domain/services/content/search/search.go
@@ -0,0 +1,52 @@
+package search
+
+import (
+	"context"
+
+	"github.com/hahaclassic/orpheon/backend/internal/domain/entity"
+	usecase "github.com/hahaclassic/orpheon/backend/internal/domain/usecases/search"
+	"github.com/hahaclassic/orpheon/backend/pkg/errwrap"
+)
+
+type SearchRepository interface {
+	SearchTracks(ctx context.Context, req *entity.SearchRequest) ([]*entity.TrackMeta, error)
+	SearchAlbums(ctx context.Context, req *entity.SearchRequest) ([]*entity.AlbumMeta, error)
+	SearchArtists(ctx context.Context, req *entity.SearchRequest) ([]*entity.ArtistMeta, error)
+	SearchPlaylists(ctx context.Context, req *entity.SearchRequest) ([]*entity.PlaylistMeta, error)
+}
+
+type SearchService struct {
+	repo SearchRepository
+}
+
+func (s *SearchService) SearchTracks(ctx context.Context, req *entity.SearchRequest) (_ []*entity.TrackMeta, err error) {
+	defer func() {
+		err = errwrap.WrapIfErr(usecase.ErrSearchTracks, err)
+	}()
+
+	return s.repo.SearchTracks(ctx, req)
+}
+
+func (s *SearchService) SearchAlbums(ctx context.Context, req *entity.SearchRequest) (_ []*entity.AlbumMeta, err error) {
+	defer func() {
+		err = errwrap.WrapIfErr(usecase.ErrSearchAlbums, err)
+	}()
+
+	return s.repo.SearchAlbums(ctx, req)
+}
+
+func (s *SearchService) SearchArtists(ctx context.Context, req *entity.SearchRequest) (_ []*entity.ArtistMeta, err error) {
+	defer func() {
+		err = errwrap.WrapIfErr(usecase.ErrSearchArtists, err)
+	}()
+
+	return s.repo.SearchArtists(ctx, req)
+}
+
+func (s *SearchService) SearchPlaylists(ctx context.Context, req *entity.SearchRequest) (_ []*entity.PlaylistMeta, err error) {
+	defer func() {
+		err = errwrap.WrapIfErr(usecase.ErrSearchPlaylists, err)
+	}()
+
+	return s.repo.SearchPlaylists(ctx, req)
+}
