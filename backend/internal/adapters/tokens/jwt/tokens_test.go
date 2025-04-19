1372e25bdc140ee3afd994ca27a35a37a1eb39a4
hahaclassic <gavrilyukvlad221@gmail.com>
2025-04-19 21:23:43 +0300

    feature/business-layer: fixed jwt test

--- backend/internal/adapters/tokens/jwt/tokens_test.go
+++ backend/internal/adapters/tokens/jwt/tokens_test.go
@@ -50,7 +50,7 @@ func TestTokenExpired(t *testing.T) {
 	token, err := tokenService.GenerateAccessToken(claims)
 	assert.NoError(t, err)
 
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second*2 + time.Millisecond*500)
 
 	_, err = tokenService.ParseAccessToken(token)
 	assert.Error(t, err)
