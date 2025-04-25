e69b04a382d847de5e35b2a69d8c68537ef4d126
hahaclassic <gavrilyukvlad221@gmail.com>
2025-04-25 16:47:00 +0300

    feature/business-layer: updated local unit testing scripts

---
+++ backend/scripts/cov.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+go test -covermode=atomic -coverprofile=./coverage/coverage.out ./internal/... > coverage/result.log
+
+if [ $? -eq 0 ]; then
+    if [[ "$1" == "-v" ]]; then 
+        cat coverage/result.log
+    fi
+    coverage=$(go tool cover -func=./coverage/coverage.out | grep "total:" | awk '{print $3}')
+    echo "status: OK. coverage: $coverage"
+else
+    echo "status: FAIL"
+    cat coverage/result.log
+fi
