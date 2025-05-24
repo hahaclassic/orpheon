7b7a6a7621bd5d52b84fbd14de50a78fde8cf041
hahaclassic <gavrilyukvlad221@gmail.com>
2025-05-24 17:13:14 +0300

    feature/deploy-beta: try to fix backend host on web

--- frontend/web/src/presentation/services/api.ts
+++ frontend/web/src/presentation/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { AxiosResponse, AxiosInstance, AxiosRequestConfig } from 'axios';
 
-const API_URL = 'http://localhost:8080/api/v1';
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface User {
   id: string;
