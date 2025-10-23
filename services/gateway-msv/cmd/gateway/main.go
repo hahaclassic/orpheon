5259aaee1835edd9d0b27312afa7c65de0ad4fa8
hahaclassic <gavrilyukvlad221@gmail.com>
2025-10-23 18:49:42 +0300

    fix(api): added 404 status codes, changed /me to /users/me route, etc

--- services/gateway-msv/cmd/gateway/main.go
+++ services/gateway-msv/cmd/gateway/main.go
@@ -11,36 +11,35 @@ import (
 func main() {
 	r := gin.Default()
 
-	// Определяем абсолютные пути к YAML файлам
-	projectRoot := "../../" // путь от cmd/gateway/main.go до корня проекта
-	authOpenAPIPath := filepath.Join(projectRoot, "api/gateway/auth/v2/openapi.yaml")
-	contentOpenAPIPath := filepath.Join(projectRoot, "api/gateway/content/v2/openapi.yaml")
-	userOpenAPIPath := filepath.Join(projectRoot, "api/gateway/user/v2/openapi.yaml")
-	streamingOpenAPIPath := filepath.Join(projectRoot, "api/gateway/streaming/v2/openapi.yaml")
+	projectRoot := "../../"
+	authOpenAPIPath := filepath.Join(projectRoot, "api/gateway-msv/auth/v2/openapi.yaml")
+	contentOpenAPIPath := filepath.Join(projectRoot, "api/gateway-msv/content/v2/openapi.yaml")
+	userOpenAPIPath := filepath.Join(projectRoot, "api/gateway-msv/user/v2/openapi.yaml")
+	streamingOpenAPIPath := filepath.Join(projectRoot, "api/gateway-msv/streaming/v2/openapi.yaml")
 
 	// Эндпоинты отдачи YAML
-	r.GET("/api/gateway/auth/v2/openapi.yaml", func(c *gin.Context) {
+	r.GET("/api/gateway-msv/auth/v2/openapi.yaml", func(c *gin.Context) {
 		c.File(authOpenAPIPath)
 	})
-	r.GET("/api/gateway/content/v2/openapi.yaml", func(c *gin.Context) {
+	r.GET("/api/gateway-msv/content/v2/openapi.yaml", func(c *gin.Context) {
 		c.File(contentOpenAPIPath)
 	})
-	r.GET("/api/gateway/user/v2/openapi.yaml", func(c *gin.Context) {
+	r.GET("/api/gateway-msv/user/v2/openapi.yaml", func(c *gin.Context) {
 		c.File(userOpenAPIPath)
 	})
-	r.GET("/api/gateway/streaming/v2/openapi.yaml", func(c *gin.Context) {
+	r.GET("/api/gateway-msv/streaming/v2/openapi.yaml", func(c *gin.Context) {
 		c.File(streamingOpenAPIPath)
 	})
 
 	// Swagger UI для каждого сервиса
 	r.GET("/swagger/auth/*any", ginSwagger.WrapHandler(swaggerFiles.Handler,
-		ginSwagger.URL("/api/gateway/auth/v2/openapi.yaml")))
+		ginSwagger.URL("/api/gateway-msv/auth/v2/openapi.yaml")))
 	r.GET("/swagger/content/*any", ginSwagger.WrapHandler(swaggerFiles.Handler,
-		ginSwagger.URL("/api/gateway/content/v2/openapi.yaml")))
+		ginSwagger.URL("/api/gateway-msv/content/v2/openapi.yaml")))
 	r.GET("/swagger/user/*any", ginSwagger.WrapHandler(swaggerFiles.Handler,
-		ginSwagger.URL("/api/gateway/user/v2/openapi.yaml")))
+		ginSwagger.URL("/api/gateway-msv/user/v2/openapi.yaml")))
 	r.GET("/swagger/streaming/*any", ginSwagger.WrapHandler(swaggerFiles.Handler,
-		ginSwagger.URL("/api/gateway/streaming/v2/openapi.yaml")))
+		ginSwagger.URL("/api/gateway-msv/streaming/v2/openapi.yaml")))
 
 	// Healthcheck
 	r.GET("/status", func(c *gin.Context) {
