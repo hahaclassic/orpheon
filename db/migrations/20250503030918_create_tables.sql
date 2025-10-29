3899ce86cc8c1012fefe9e267998750327651870
hahaclassic <gavrilyukvlad221@gmail.com>
2025-10-30 01:03:31 +0300

    upd(user-scheme): deleted birth_date from user scheme

--- db/migrations/20250503030918_create_tables.sql
+++ db/migrations/20250503030918_create_tables.sql
@@ -16,7 +16,6 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     name TEXT UNIQUE NOT NULL CHECK (length(name) > 2), -- Имя должно быть хотя бы 3 символа
     registration_date TIMESTAMP NOT NULL DEFAULT NOW(), -- Дата регистрации по умолчанию
-    birth_date DATE CHECK (birth_date < registration_date), -- Дата рождения не может быть в будущем
     access_level INT NOT NULL CHECK (access_level IN (1, 2)) -- Ограничиваем возможные роли
 );
 
