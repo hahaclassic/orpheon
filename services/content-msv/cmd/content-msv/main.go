ffe3229cde5f392728445553606333f225fe7e40
hahaclassic <gavrilyukvlad221@gmail.com>
2026-01-24 03:29:17 +0300

    content-msv: fixed import

--- services/content-msv/cmd/content-msv/main.go
+++ services/content-msv/cmd/content-msv/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hahaclassic/orpheon/pkg/config"
 	contentcfg "github.com/hahaclassic/orpheon/services/content-msv/config"
-	app "github.com/hahaclassic/orpheon/services/content-msv/internal/app/http"
+	"github.com/hahaclassic/orpheon/services/content-msv/internal/app"
 )
 
 func main() {
